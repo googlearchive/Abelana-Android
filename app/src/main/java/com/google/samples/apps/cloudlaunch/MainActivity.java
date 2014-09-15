413ce6da56311359031aec9ddba4ec5a5129bd74
Zafir Khan <zafir.khan1@gmail.com>
2014-09-15 10:07:46 -0700

    test commit

--- app/src/main/java/com/google/samples/apps/cloudlaunch/MainActivity.java
+++ app/src/main/java/com/google/samples/apps/cloudlaunch/MainActivity.java
@@ -1,7 +1,6 @@
 package com.google.samples.apps.cloudlaunch;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 
@@ -31,6 +29,8 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        int test = 1;
+        test += 1;
         return true;
     }
 

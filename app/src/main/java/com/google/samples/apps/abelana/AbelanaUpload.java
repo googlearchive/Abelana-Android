34944aaec6080921773e4fa24e9c3a3d0a5b3477
Zafir Khan <zafir.khan1@gmail.com>
2014-10-30 21:30:08 -0700

    Fixed logout backstack issue. Added headers.

--- app/src/main/java/com/google/samples/apps/abelana/AbelanaUpload.java
+++ app/src/main/java/com/google/samples/apps/abelana/AbelanaUpload.java
@@ -1,11 +1,27 @@
+/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.samples.apps.abelana;
+
+import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.api.client.http.InputStreamContent;
 import com.google.api.services.storage.Storage;
 import com.google.api.services.storage.model.StorageObject;
-
-import android.os.AsyncTask;
-import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
